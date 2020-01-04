c63c3bc2ce26dbc54be910f1f65710e5bccd9d03
xomyachok_shaolin <xomyackok8@gmail.com>
2020-01-05 01:08:17 +0500

    Пример использования нейронной сети

--- NeuralNetworksTests/NeuralNetworkTests.cs
+++ NeuralNetworksTests/NeuralNetworkTests.cs
@@ -17,7 +17,7 @@ namespace NeuralNetworks.Tests {
             neuralNetwork.Layers[1].Neurons[1].SetWeights(0.1, -0.3, 0.7, -0.3);
             neuralNetwork.Layers[2].Neurons[0].SetWeights(1.2, 0.8);
 
-            //var result = neuralNetwork.FeedForward(new List<double> { 1, 0, 0, 0 });
+            // var result = neuralNetwork.FeedForward(new List<double> { 1, 0, 0, 0 });
         }
     }
 }
